52f78d1f57fbec0a0d9ca16d527b01c825b9bf61
Ferdhika Yudira <rpl4rt08@gmail.com>
2020-12-20 23:03:57 +0700

    :sparkles: add sample function

--- sample/main.go
+++ sample/main.go
@@ -7,10 +7,13 @@ import (
 	"github.com/ferdhika31/iPaymu-go"
 )
 
+var API_SECRET = "387AF695-EE0B-47BD-91A4-ADF55261159D"
+var VA = "1179003821708289"
+
 func redirPayment() {
 	var cfg = iPaymu.NewConfig()
-	cfg.ApiSecret = "387AF695-EE0B-47BD-91A4-ADF55261159D"
-	cfg.VirtualAccount = "1179003821708289"
+	cfg.ApiSecret = API_SECRET
+	cfg.VirtualAccount = VA
 	cfg.NotifyUrl = "http://localhost:8000/notify"
 	cfg.ReturnUrl = "http://localhost:8000/return"
 	cfg.CancelUrl = "http://localhost:8000/cancel"
@@ -21,8 +24,10 @@ func redirPayment() {
 	rp.AddProduct(iPaymu.Product{Name: "Pot Kayu", Qty: 1, Price: 18000})
 	rp.AddProduct(iPaymu.Product{Name: "Kaos", Qty: 2, Price: 60000})
 
+	// optional
 	payload := map[string]interface{}{
-		"referenceId": "TRX0031",
+		"referenceId":   "TRX0031",
+		"paymentMethod": "qris",
 	}
 
 	res := rp.Create(payload)
@@ -35,6 +40,23 @@ func redirPayment() {
 	fmt.Println(string(jsonString))
 }
 
+func getBalance() {
+	var cfg = iPaymu.NewConfig()
+	cfg.ApiSecret = API_SECRET
+
+	b := iPaymu.Balance{Config: cfg}
+
+	res := b.GetBalance()
+
+	jsonString, err := json.Marshal(res)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(jsonString))
+}
+
 func main() {
 	redirPayment()
+	getBalance()
 }
