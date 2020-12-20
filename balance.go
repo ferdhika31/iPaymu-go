6045bbd63064bf6be71b0a9a392c0d52d8f01057
Ferdhika Yudira <rpl4rt08@gmail.com>
2020-12-20 23:03:40 +0700

    :sparkles: get balance feature

---
+++ balance.go
@@ -0,0 +1,11 @@
+package iPaymu
+
+type Balance struct {
+	Config Config
+}
+
+func (b *Balance) GetBalance() map[string]interface{} {
+	res := Request("GET", "/api/saldo?key="+b.Config.ApiSecret, nil, b.Config)
+
+	return res
+}
