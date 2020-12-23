303ddfb3879e5cbf4b098ef2f7bbe0d661a71652
Ferdhika Yudira <rpl4rt08@gmail.com>
2020-12-23 08:38:42 +0700

    :hammer: request direct

--- request.go
+++ request.go
@@ -1,8 +1,20 @@
 package ipaymu
 
+import "github.com/ferdhika31/iPaymu-go/consts"
+
 type RedirectRequest struct {
-	ReferenceID   string            `json:"referenceId,omitempty"`
-	PaymentMethod PaymentMethodType `json:"paymentMethod,omitempty"`
+	ReferenceID   string                   `json:"referenceId,omitempty"`
+	PaymentMethod consts.PaymentMethodType `json:"paymentMethod,omitempty"`
+}
+
+type DirectRequest struct {
+	Amount         uint64                    `json:"amount,omitempty"`
+	Expired        int64                     `json:"expired,omitempty"`
+	ExpiredType    consts.ExpiredType        `json:"expiredType,omitempty"`
+	Comments       string                    `json:"comments,omitempty"`
+	ReferenceID    string                    `json:"referenceId,omitempty"`
+	PaymentMethod  consts.PaymentMethodType  `json:"paymentMethod,omitempty"`
+	PaymentChannel consts.PaymentChannelType `json:"paymentChannel,omitempty"`
 }
 
 type CashOnDelivery struct {
