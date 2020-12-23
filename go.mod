23bbc0e394f1c7629f197593f5ad4aa6bd7fbbd7
Ferdhika Yudira <rpl4rt08@gmail.com>
2020-12-23 08:05:31 +0700

    :sparkles: change struct to map

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/ferdhika31/iPaymu-go
 
 go 1.15
-
-require github.com/fatih/structs v1.1.0
--- helpers.go
+++ helpers.go
@@ -4,6 +4,7 @@ import (
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/hex"
+	"encoding/json"
 	"io"
 	"log"
 	"strings"
@@ -32,4 +33,18 @@ func StringHMACSha256(data string, secret string) string {
 	sha := hex.EncodeToString(h.Sum(nil))
 
 	return sha
+}
+
+// from https://gist.github.com/bxcodec/c2a25cfc75f6b21a0492951706bc80b8#gistcomment-3381061
+func structToMap(data interface{}) (map[string]interface{}, error) {
+	dataBytes, err := json.Marshal(data)
+	if err != nil {
+		return nil, err
+	}
+	mapData := make(map[string]interface{})
+	err = json.Unmarshal(dataBytes, &mapData)
+	if err != nil {
+		return nil, err
+	}
+	return mapData, nil
 }
